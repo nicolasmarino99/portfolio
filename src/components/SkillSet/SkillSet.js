a2044516937fbfcf9b10c7e5376b02434c53f235
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2021-02-02 18:17:16 -0500

    add styles to skills component

--- src/components/SkillSet/SkillSet.js
+++ src/components/SkillSet/SkillSet.js
@@ -31,7 +31,7 @@ const SkillSet = () => {
                 {skills.map(item => <SkillCard item={item}/>)}
             </div>
             <div className="info-cv">
-              <div className="number" id="n1">03</div>
+              <div className="number" id="n3">03</div>
               <p className="greeting-p">Some words</p>
               <h1 id="name">About me</h1>
               <p className="presentaion-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
